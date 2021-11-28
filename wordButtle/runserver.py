ee5678bc23efb358db2ffe6546a88c378339e29b
hadash123 <nias.adamov@gmail.com>
2021-11-29 01:07:48 +0200

    linux update

--- wordButtle/runserver.py
+++ wordButtle/runserver.py
@@ -6,4 +6,4 @@ from os import environ
 from wordButtle import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app,host = "localhost", port = 8080)
+    socketio.run(app,host = "0.0.0.0", port = 5000)
